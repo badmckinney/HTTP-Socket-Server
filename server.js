222e892478342d4ac6d5998f61912940891b1acd
Brad McKinney <brad.mckin@gmail.com>
2019-01-19 20:58:48 -1000

    creates a server and routes for our in-memory files

---
+++ server.js
@@ -0,0 +1,53 @@
+const file404 = require('./404.js');
+const helium = require('./helium.js');
+const hydrogen = require('./hydrogen.js');
+const index = require('./index.js');
+const styles = require('./styles');
+const net = require('net');
+
+let contentType = "text/html; charset=utf-8";
+let body = "";
+let date = "";
+
+// this creates a server
+const server = net.createServer((socket) => {
+  socket.setEncoding('utf8');
+  socket.on('data', (data) => {
+    let URI = data.slice(4, data.indexOf("H") - 1);
+
+    if (URI === "/" || URI === "/index.html") {
+      body = index.index;
+    } else if (URI === "/hydrogen.html") {
+      body = hydrogen.hydrogen;
+    } else if (URI === "/helium.html") {
+      body = helium.helium;
+    } else if (URI === "/css/styles.css") {
+      body = styles.styles;
+      contentType = "text/css";
+    } else {
+      body = file404.file404;
+    }
+    date = new Date().toUTCString();
+
+    let response = `
+HTTP/1.1 200 OK
+Server: skynet/4.1.15
+Date: ${date}
+Content-Type: ${contentType}
+Content-Length: ${body.length}
+
+${body}
+`;
+
+    socket.end(response);
+  });
+})
+  // handle errors on the server
+  .on('error', (err) => {
+    console.log(err);
+  });
+
+// this starts the server
+server.listen(8080, () => {
+  console.log('Server is UP');
+});
